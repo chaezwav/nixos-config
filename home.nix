266c995542bc0d8ad62e19810865d334dfcad051
Koehn Humphries <koehnh@icloud.com>
2024-08-17 17:48:23 -0400

    Added git signing

--- home.nix
+++ home.nix
@@ -53,8 +53,13 @@
   
   programs.git = {
     enable = true;
-    userName = "chaezwav";
+    userName = "Koehn Humphries";
     userEmail = "koehnh@icloud.com";
+    extraConfig = {
+        gpg.format = "ssh";
+        commit.gpgsign = true;
+        user.signingkey = "~/.ssh/id_ed25519.pub";
+    };
   };
   
   home.packages = with pkgs; [
